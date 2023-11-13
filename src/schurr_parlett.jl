af6706f580c8698fa5c118fa0cc8acdd7e42b8c6
Aravindh Krishnamoorthy <aravindh.krishnamoorthy@fau.de>
2023-11-13 20:47:32 +0100

    Updates

--- src/schurr_parlett.jl
+++ src/schurr_parlett.jl
@@ -1,9 +1,12 @@
 # Schur-Parlett algorithm based on Algorithm 9.6 in Matrix Functions, Higham.
 function fm_schur_parlett(f::Function, X::AbstractMatrix)
-    T, Q = schur(X)
+    S = schur(X)
     # For diagonalisable matrices, use the straightforward formula
-    if isdiag(T)
-        F = Q*f.(T)*Q'
+    if isdiag(S.T)
+        F = S.Z*f.(S.T)*S.Z'
         return F
+    elseif !istriu(S.T)
+        # T is only triangular in the complex field
+        S = Schur{Complex}(S)
     end
 end
